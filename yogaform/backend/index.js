b58e41001177871b0feaff657da586ae8c6cffa6
Shashwat Gandhi <shashwatgandhi88@gmail.com>
2022-12-12 20:23:04 +0530

    change to serving over express static

--- yogaform/backend/index.js
+++ yogaform/backend/index.js
@@ -7,11 +7,18 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
 require('dotenv').config()
 
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port: 5000')
-})
 
 app.get('/', (req,res) => {
     console.log('works')
     res.send('ok')
 })
+
+app.use(express.static(path.join(__dirname, "../frontend/build")))
+app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Listening on port: 5000')
+})
+export default app;
