f0e27f2a22ad369b16309d5bc384072121636f05
Shashwat Gandhi <shashwatgandhi88@gmail.com>
2022-12-12 20:34:07 +0530

    reverted back from express static

--- yogaform/backend/index.js
+++ yogaform/backend/index.js
@@ -13,10 +13,6 @@ app.get('/', (req,res) => {
     res.send('ok')
 })
 
-app.use(express.static(path.join(__dirname, "../frontend/build")))
-app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
-})
 
 app.listen(process.env.PORT, () => {
     console.log('Listening on port: 5000')
